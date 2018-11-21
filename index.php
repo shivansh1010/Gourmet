2f58defb9eb9fe9ab96d11a13108a2c60b5b99ab
Shivansh <shivanshdhiman@gmail.com>
2018-11-21 12:51:41 +0530

    Home page added | Simple search

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+ ?>
+
+ <html>
+    <head>
+        <title>Gourmet</title>
+        <link type="text/css" rel="stylesheet" href="css/header.css" />
+    </head>
+    <body topmargin=0 leftmargin=0>
+        <div class="header">
+            <div class="header_top" >
+                <div class="left_buttons">
+                    <a href="#">Cuisines</a><br>
+                    <a href="#">Restaurents</a>
+                </div>
+                <div class="right_buttons">
+                    <form method="get" action="search.php" name="search">
+                        <select name="search_type">
+                            <option value="food">Food</option>
+                            <option value="rest">Restaurent</option>
+                        </select>
+                        <input type="text" name="keyword" >
+                        <input type="submit" value="Search">
+                    </form>
+                    <a href="#">login</a><br>
+                    <a href="#">signup</a>
+                </div>
+            </div>
+            <div class="disc_one">
+
+            </div>
+            <div class="disc_two">
+            </div>
+            <div class="gourmet_logo" ></div>
+
+        </div>
+
+
+        <div class="second">
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        </div>
+    </body>
+ </html>
