39f52f813a03f93cc6d898c0d7a2adac46f00963
Shivansh <shivanshdhiman@gmail.com>
2018-11-21 22:16:13 +0530

    Homepage added

--- index.php
+++ index.php
@@ -5,39 +5,47 @@
     <head>
         <title>Gourmet</title>
         <link type="text/css" rel="stylesheet" href="css/header.css" />
+        <link type="text/css" rel="stylesheet" href="css/index.css" />
     </head>
     <body topmargin=0 leftmargin=0>
         <div class="header">
             <div class="header_top" >
                 <div class="left_buttons">
-                    <a href="#">Cuisines</a><br>
-                    <a href="#">Restaurents</a>
+                    <a href="#" class="header_hover" >CUISINES</a>
+                    <a href="#">RESTAURANT</a>
                 </div>
                 <div class="right_buttons">
-                    <form method="get" action="search.php" name="search">
-                        <select name="search_type">
-                            <option value="food">Food</option>
-                            <option value="rest">Restaurent</option>
-                        </select>
-                        <input type="text" name="keyword" >
-                        <input type="submit" value="Search">
-                    </form>
-                    <a href="#">login</a><br>
-                    <a href="#">signup</a>
+                    <a href="#login">LOGIN</a>
+                    <a href="#">SIGNUP</a>
                 </div>
             </div>
-            <div class="disc_one">
 
-            </div>
-            <div class="disc_two">
-            </div>
-            <div class="gourmet_logo" ></div>
+            <form class="search" name="search" action="logincheck.php" method="get">
+                <select name="search_type">
+                    <option value="food">FOOD</option>
+                    <option value="rest">RESTAURANT</option>
+                </select>
+                <input type="search" placeholder="  TYPE" name="keyword" required>
+                <button type="submit">SEARCH</button>
+            </form>
 
+        </div>
+        <div class="headerbg"></div>
+
+        <div class="blurbg"></div>
+        <div class="login" >
+            <a name="login"></a>
+            <h1>LOGIN</h1>
+            <form class="loginform" name="search" action="search.php" method="post">
+                <input type="text" placeholder="  USERNAME" name="id" required>
+                <input type="password" placeholder="  PASSWORD" name="pwd" required>
+                <button type="submit">LOGIN</button>
+            </form>
         </div>
 
 
         <div class="second">
-        skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
+        !!!!!!dfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
         skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
         skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
         skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>skjdskdfkfsff<br>
