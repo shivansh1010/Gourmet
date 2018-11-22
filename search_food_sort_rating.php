c1d9bfd4158be136d3292cf69cac9d22a52efd99
anshuman16423 <38358951+anshuman16423@users.noreply.github.com>
2018-11-22 12:40:41 +0530

    abc

---
+++ search_food_sort_rating.php
@@ -0,0 +1,16 @@
+<?php
+  include("PhpMysqlConnectivity.php");
+  $food_name="pig";//form data here
+  $result=mysqli_query($link,"SELECT DISTINCT a.name,c.name,b.star,a.type,b.price,c.city FROM food a, serves b,restaurant c WHERE a.id=b.f_id AND b.r_id=c.id AND a.name='$food_name' AND CAST(CURRENT_TIME() AS time) BETWEEN c.open_time AND c.close_time AND a.name='pig' ORDER BY b.star ;");
+  while($row = mysqli_fetch_array($result))
+  {
+
+    foreach ($row as $key => $value) {
+        echo $value." ";
+    }
+    echo "<br>";
+    //It Works Printing each value;
+    //pata he be
+  }
+
+ ?>
