fe3fab8c31133ab0d69769a66a292ecc5c221f9c
Janglee123 <2017180@iiitdmj.ac.in>
2018-11-22 19:53:12 +0530

    Meru

--- search_food.php
+++ search_food.php
@@ -13,12 +13,19 @@
     $result = mysqli_query($link,"SELECT DISTINCT f.name,r.name,s.star,f.type,s.price,r.city FROM food f, serves s,restaurant r WHERE f.id = s.f_id AND s.r_id = r.id AND f.name = '$name' AND CAST(CURRENT_TIME() AS time) BETWEEN r.open_time AND r.close_time ORDER BY s.price;");    
   }
   
+  echo '<table>';
+  echo '<th>Name</th><th>Star</th><th>Type</th><th>Price</th><th>City</th>';
   while($row = mysqli_fetch_array($result))
   { 
-    print_r($row);
-    //echo $row;
-    echo "";//."$row['name'] $row['star'] $row['type'] $row['price'] $row['city']";
-    echo "<br>";
+    echo '<tr>';
+    echo '<td>'.$row["name"].'</td>';//.$row['star'].$row['type'].$row['price'].$row['city'];
+    echo '<td>'.$row["star"].'</td>';
+    echo '<td>'.$row["type"].'</td>';
+    echo '<td>'.$row["price"].'</td>';
+    echo '<td>'.$row["city"].'</td>';
+    echo '</tr>';
   }
+  echo '</table>'
+
 
  ?>
