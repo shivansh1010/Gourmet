900a5181fa8a3b582c1620a202ccd00caad626f0
Janglee123 <2017180@iiitdmj.ac.in>
2018-11-22 21:42:07 +0530

    add_user added

---
+++ add_user.php
@@ -0,0 +1,39 @@
+<?php
+
+    include("PhpMysqlConnectivity.php");
+
+    $name = $_POST['name'];
+    $pswd = $_POST['pswd'];
+    $town = $_POST['town'];
+    $mobno = $_POST['mobno'];
+    
+    echo "bas bhai";
+    try{
+
+        $q = "INSERT INTO `user`(`id`, `pswd`, `city`, `mobile_no`) VALUES ('$name','$pswd','$town','$mobno')";
+        
+        $result = mysqli_query($link,$q);
+        
+        
+        if(!$result){
+        
+            $error = mysqli_error($link);
+            print($error);
+
+            if(strpos($error,"Duplicate")>=0){
+               header('Location: '.'signup.php/?user_found=true');
+            }
+            if(strpos($error,"'mobile_no'")>0){
+               header('Location: '.'signup.php/?invalid_no=true');    
+            }
+            
+        }
+        
+        //header('Location: '.'index.php');            
+        
+    }
+    catch(Exception $e){
+        print('Something, somewhere is wrong'.mysqli_error($link));
+    }
+
+?>
--- signup.php
+++ signup.php
@@ -1,4 +1,19 @@
 <?php
+    /*if( $_GET && $_GET['user_found'] && $_GET['user_found']==true){
+    }
+    if($_GET && $_GET['invalid_no']==true){
+    }*/
+    
+    foreach ($_GET as $key => $value) {
+        if ($key == 'user_found' && $value) {
+            echo "<h1>User Exists</h1>";   
+        }
+    	if ($key == 'invalid_no' && $value) {
+            echo "<h1>Enter Valid Mobile No </h1>";
+    	}
+    }
+
+    
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +26,9 @@
 	<form action="add_user.php" method="post">
 		name <input type="text" name="name"><br>
 		password <input type="text" name="pswd"><br>
-		mobile no<input type="text" name="mno"><br>
+		Town <input type="text" name="town"><br>
+        mobile no: +91 <input type="text" name="mobno"><br>
+        <input type="submit" value="Submit">
 	</form>
 </body>
 
