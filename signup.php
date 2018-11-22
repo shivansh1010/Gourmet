f9b542fc5321029920a3f854809e5b709da5ad38
Janglee123 <2017180@iiitdmj.ac.in>
2018-11-22 20:26:33 +0530

    sign up

---
+++ signup.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up | Gourmet</title>
+</head>
+
+<body>
+	<form action="add_user.php" method="post">
+		name <input type="text" name="name"><br>
+		password <input type="text" name="pswd"><br>
+		mobile no<input type="text" name="mno"><br>
+	</form>
+</body>
+
+</html>
