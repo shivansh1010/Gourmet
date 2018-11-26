9d4cd4a46edbe743d308a9324d817444ff9f6401
Shivansh <shivanshdhiman@gmail.com>
2018-11-27 01:54:58 +0530

    booking.php done

--- booking.php
+++ booking.php
@@ -1,9 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Booking | Gourmet</title>
+	<title>Sign Up | Gourmet</title>
+    <link type="text/css" rel="stylesheet" href="css/signup.css" />
+</head>
+
+</head>
+
+<body>
+<div class="header">
+	<div class="header_top" >
+		<div class="left_buttons">
+		  <a href="./food_list.php" class="header_hover" >Cuisines</a>
+		  <a href="./rest_list.php">Restaurant</a>
+		</div>
+		<div class="right_buttons">
+			<?php
+			  include('login_buttons.php');
+			?>
+		</div>
+	</div>
+</div>
+<div class="headerbg"></div>
     <?php
-        session_start();
+        //session_start();
         include('PhpMysqlConnectivity.php');
         if (!isset($_SESSION['u_id'])) {
             header('Location: '.'./#login_user');
@@ -11,7 +31,7 @@
 
         $u_id = $_SESSION['u_id'];
         $u_name = $_SESSION['u_name'];
-        
+
         if(isset($_POST['r_id'])){
             $r_id = $_POST['r_id'];
         }
@@ -28,38 +48,43 @@
         $r_seats = $result[10];
         $r_address = $result[2];
     ?>
-</head>
-<body>
-    <?php 
+<center>
+    <?php
+    ?>
+    <form action='./book_seat.php' method="POST" class="formx">
+		<div class="formbgcolor">
+    <?php
+	/*
         echo '<div> Name : '.$r_name.'</div>';
         echo '<div> Total no of seats : '.$r_seats.'</div>';
         echo '<div> Address : '.$r_address.'</div>';
 
-        echo '<br><div> User :- '.$u_name.'</div>';
+        echo '<br><div> User :- '.$u_name.'</div>';*/
         echo '<br>';
-    ?>
-    <form action='./book_seat.php' method="POST">
-    <?php  
             echo '<input type = "hidden" name = "r_id" value = '.$r_id.'>';
             echo '<input type = "hidden" name = "r_name" value = '.$r_name.'>';
-           
-            if (isset($_POST['res']) && $_POST['res']) {       
-                echo 'Number of seats <input type="number" name="no" min=1 value='.$_POST['no'].' readonly ><br>';
-                echo 'Time <input type="time" name="start_time" value='.$_POST['st'].' readonly >';
-                echo '<input type="time" name="end_time" value='.$_POST['et'].' readonly ><br>';
-                echo 'Seat Avalable Book Now !!';
-                echo '<input type="submit" value="Book" name="book">';
-            } 
+
+            if (isset($_POST['res']) && $_POST['res']) {
+                echo '<label><input class="readonly" type="number" name="no" min=1 value='.$_POST['no'].' readonly ><div class="label-text">Seats required</div></label>';
+                echo '<label><input class="readonly" type="time" name="start_time" value='.$_POST['st'].' readonly ><div class="label-text">Start Time</div></label> ';
+                echo '<label><input class="readonly" type="time" name="end_time" value='.$_POST['et'].' readonly ><div class="label-text">End Time</div></label>';
+                echo '<br><div style="color:rgba(100, 255, 65, 1);">Seats are Available. Book Now !!</div>';
+                echo '<button type="submit" value="Book" name="book">Book Table</button><br><br>';
+            }
             else{
                 if(isset($_POST['res'])){
-                    echo 'Seats are not avalable';
+                    echo 'Seats are not available.';
                 }
-                echo 'Number of seats <input type="number" name="no" min=1 max='.$r_seats.' value=1 required ><br>';
-                echo 'Time <input type="time" name="start_time" required>';
-                echo '<input type="time" name="end_time" required><br>';
-                echo '<input type="submit" value="Check" name="check">';
+                echo '<label><input type="number" name="no" min=1 max='.$r_seats.' value=1 required ><div class="label-text">Seats required</div></label>';
+                echo '<label><input class="time" type="time" name="start_time" required><div class="label-text">Start Time</div></label>';
+                echo '<label><input class="time" type="time" name="end_time" required><div class="label-text">End Time</div></label>';
+                echo '<button type="submit" value="Check" name="check">Check Availability</button><br><br>';
             }
-        ?>
+    ?>
+		</div>
     </form>
+</center>
+<br><br>
 </body>
+
 </html>
--- login_buttons.php
+++ login_buttons.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     if (isset($_SESSION['u_id'])) {
+        ///echo '<a href="index.php"><div style="position:absolute;width:230px;height:150px;z-index:100;left:-330px;"></div></a>';
         echo '<a href="./logout.php">Log Out</a>';
         echo '<a href="./user.php">'.$_SESSION["u_id"].'</a>';
 
